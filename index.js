18790ccf99b22775e4ae37acec73b2d7e747b592
Tomas Riat <tomasiriat@hotmail.com>
2022-10-06 22:20:06 -0300

    modif

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
+const button__contact1 = document.getElementById('button__contact1')
+
+
 const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
 function changetheme(ev){
     if(ev.target.checked){
@@ -12,6 +15,16 @@ colorSwitch.addEventListener('change', changetheme);
 
 localStorage.setItem('change', changetheme);
 
+
+button__contact1.addEventListener('click',() =>{
+    Swal.fire({
+        
+        showConfirmButton: false,
+        width: '600px',
+        html: '<h1 class="h1__sweetAlert">Message sent!</h1><p class="p__sweetAlert">Thanks for contacting me, I’ll be aswering soon.</p><br><br><a href="../index.html" class="button-sweetAlert"><i class=" arrow_sweet bi bi-arrow-left"></i>Back to Home</a><br> <br>'
+
+    })
+})
 
 
 
