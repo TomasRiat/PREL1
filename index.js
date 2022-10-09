66bd431692dab20eb4414621fdea8c00270fbf08
Tomas Riat <tomasiriat@hotmail.com>
2022-10-09 11:55:29 -0300

    modif

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
+const toggle = document.getElementById('toggleDark');
+const button = document.getElementById('buttonContact');
 
-const button__contact1 = document.getElementById('button__contact1')
-
-
-const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
-function changetheme(ev){
-    if(ev.target.checked){
-        document.documentElement.setAttribute('theme', 'light');
-    } else {
-        document.documentElement.setAttribute('theme', 'dark');
+toggle.addEventListener('click', function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        toggle.src = "../assets/img/sun.png"
+    }else{
+        toggle.src = "../assets/img/moon.png"
     }
-}
-colorSwitch.addEventListener('change', changetheme);
+});
 
 
-localStorage.setItem('change', changetheme);
 
-
-button__contact1.addEventListener('click',() =>{
+button.onclick('click', () =>{
     Swal.fire({
-        
         showConfirmButton: false,
         width: '600px',
         html: '<h1 class="h1__sweetAlert">Message sent!</h1><p class="p__sweetAlert">Thanks for contacting me, I’ll be aswering soon.</p><br><br><a href="../index.html" class="button-sweetAlert"><i class=" arrow_sweet bi bi-arrow-left"></i>Back to Home</a><br> <br>'
-
-    })
-})
+    });
+});
 
 
 
