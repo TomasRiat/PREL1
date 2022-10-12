80454b2db46e51d0e4311d1b3e6d1771643b183d
Tomas Riat <tomasiriat@hotmail.com>
2022-10-11 21:29:29 -0300

    modif

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const toggle = document.getElementById('toggleDark');
+const btnSwitch = document.getElementById('switch');
 const button = document.getElementById('buttonContact');
 
-toggle.addEventListener('click', function(){
-    document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
-        toggle.src = "../assets/img/sun.png"
-    }else{
-        toggle.src = "../assets/img/moon.png"
+
+
+btnSwitch.addEventListener("click", () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');
     }
 });
+
+    if (localStorage.getItem('dark-mode') === 'true') {
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+    } else {
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+    }
+
 
 
 
